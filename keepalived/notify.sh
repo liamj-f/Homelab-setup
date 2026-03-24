2af4f4965c23781a85a42a6d2f70b237aec7148a
Liam J-F <liamjamesfagg+github@gmail.com>
2026-03-24 22:12:36 +0000

    make keepalived not kill npm

--- keepalived/notify.sh
+++ keepalived/notify.sh
@@ -13,6 +13,13 @@ case $STATE in
     docker exec pihole pihole -a enabledhcp
     ;;
   "BACKUP"|"FAULT")
+    sleep 5
+    # Re-check state before stopping - if we're now MASTER, abort
+    CURRENT_STATE=$(ip addr show | grep -q "192.168.0.5" && echo "MASTER" || echo "BACKUP")
+    if [ "$CURRENT_STATE" = "MASTER" ]; then
+      logger "keepalived notify: state changed to MASTER before stop completed, aborting"
+      exit 0
+    fi
     docker stop rsync
     docker stop nebula-sync
     docker stop nginx-proxy-manager
